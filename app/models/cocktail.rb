bf6a0b5399fc712807a060031853d6809536acef
Lucien Mollard <mollard.lucien@gmail.com>
2016-11-17 11:24:42 +0100

    validates model

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,3 +1,5 @@
 class Cocktail < ApplicationRecord
-  validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+  has_many :ingredients,  through: :doses, dependent: :destroy
+  has_many :doses
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,5 @@
 class Ingredient < ApplicationRecord
+  has_many :cocktail, through: :doses
+  has_many :doses
+  validates :name, presence: true, uniqueness: true
 end
